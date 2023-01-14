f601f3938ba94087a7799737f28ce32ec95c0ba1
Aakarshit Sharma <asdoon00@gmail.com>
2023-01-14 16:02:42 +0530

    Update

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -6,3 +6,4 @@ workers = 4
 # keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
 # certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
 accesslog = '-'
+loglevel = 'debug'
