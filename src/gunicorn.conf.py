c90ee7297b22a69959149e8d25ff0047afaeba2d
Aakarshit Sharma <asdoon00@gmail.com>
2023-01-15 00:28:55 +0530

    1. Removed unused images. 2. Added warnings when uploading file. 3. Added home page work in progress. 4. Added default gunicorn timeout of 5 minutes to avoid timeout while file upload. 5. Updated gunicorn workers to default of 3. 6. Removed unused templates from commit tree.

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -3,7 +3,8 @@ import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
 
-workers = 4
+workers = int(os.getenv("GUNICORN_WORKERS", 3))
+timeout = int(os.getenv("GUNICORN_TIMEOUT", 300))
 if not os.getenv('DISABLE_HTTPS'):
     keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
     certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
