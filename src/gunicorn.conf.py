11f683e766a3321d3914481f3eb01522599f8fae
Aakarshit Sharma <asdoon00@gmail.com>
2023-01-14 17:06:39 +0530

    Updated gunicorn.conf.py

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -4,7 +4,7 @@ import os
 bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
 
 workers = 4
-# keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
-# certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
+if not os.getenv('DISABLE_HTTPS'):
+    keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
+    certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
 accesslog = '-'
-loglevel = 'debug'
