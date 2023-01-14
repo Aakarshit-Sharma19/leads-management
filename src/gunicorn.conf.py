b42d81e497c08495d3d19f8e45ffe070607074e9
Aakarshit Sharma <asdoon00@gmail.com>
2023-01-14 15:42:06 +0530

    Removed certificate

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -3,6 +3,6 @@ from pathlib import Path
 bind = "0.0.0.0:8000"
 
 workers = 4
-keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
-certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
+# keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
+# certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
 accesslog = '-'
