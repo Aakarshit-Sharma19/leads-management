2fc7f3ba7e11f4aaf5b72cb15b716733f625ef7a
Aakarshit Sharma <asdoon00@gmail.com>
2023-01-27 17:42:46 +0530

    Updated gunicorn conf to override PORT if ENABLE_HTTPS

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import os
 
-bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
-
 workers = int(os.getenv("GUNICORN_WORKERS", 3))
 timeout = int(os.getenv("GUNICORN_TIMEOUT", 300))
 if os.getenv('ENABLE_HTTPS'):
     keyfile = str(Path(__file__).parent / "certificates" / "key.pem")
     certfile = str(Path(__file__).parent / "certificates" / "certificate.pem")
+    os.environ['POST'] = '443'
+
+bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
+
 accesslog = '-'
